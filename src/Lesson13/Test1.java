d9e9449784d5512888bdedb58711a01d70760440
Egor <sartvas@yandex.ru>
2021-05-20 22:43:33 +0300

    Lesson13: Switch statment

---
+++ src/Lesson13/Test1.java
@@ -0,0 +1,18 @@
+package Lesson13;
+
+public class Test1 {
+}
+
+class Student {
+    double grade;
+
+    Student(double grade){
+        this.grade = grade;
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
