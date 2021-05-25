84701ab1328608d6f00d1574d5ef7a39c5285738
Egor <sartvas@yandex.ru>
2021-05-25 17:57:33 +0300

    Lesson18 chapter1: Arrays.

---
+++ src/Lesson18/Arr.java
@@ -0,0 +1,30 @@
+package Lesson18;
+
+import java.util.Arrays;
+
+public class Arr {
+    public static void main(String[] args) {
+
+        String[] arr = {"Hi", "Hello", "Bonjorno"};
+
+        int[][] mass = {{2,4,5}, {4,5,6}, {5,6,7}}; //двумерные массивы
+
+        // System.out.println(mass[2][2]); // 7
+
+        int [] arr1 = {1, 9, 4, -9, 34, 2};
+        int [] arr4 = {1, 9, 4, -9, 34, 2};
+        for (int i = 0; i < arr1.length; i++){
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.println();
+        Arrays.sort(arr1);
+        for (int i = 0; i < arr1.length; i++){
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.println();
+        int index = Arrays.binarySearch(arr1, 2);
+        int index2 = Arrays.binarySearch(arr4, 2); // несортированный некорректно
+        System.out.println(index);
+        System.out.println(index2);
+    }
+}
