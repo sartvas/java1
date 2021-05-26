a45d8d531006eb09db7e02480aa7290ad0c6083b
Egor <sartvas@yandex.ru>
2021-05-26 17:53:55 +0300

    Lesson20: ArrayList start

---
+++ src/Lesson20/ArrList1.java
@@ -0,0 +1,13 @@
+package Lesson20;
+
+import java.util.ArrayList;
+
+public class ArrList1 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        ArrayList <String> list2 = new ArrayList();
+        list2.add("ok");
+        System.out.println(list2);
+
+    }
+}
