d27167d0488c8edbe9618b044e0d94f9d464532b
Egor <sartvas@yandex.ru>
2021-06-15 17:24:39 +0300

    Lesson27:Try/Catch/Finaly

--- src/Lesson27/TryCatchFinally.java
+++ src/Lesson27/TryCatchFinally.java
@@ -1,4 +1,26 @@
 package Lesson27;
 
 public class TryCatchFinally {
+    public static void main(String[] args) {
+        int array [] = {4, 8, 1};
+        System.out.println("array is created");
+
+        // try и finally не имеют параметров, сразу тело
+        try {
+            System.out.println("Block try is working");
+            System.out.println(array[1]);
+        }
+
+        // catch имеет параметры; catch-блоков может быть несколько
+        catch (ArrayIndexOutOfBoundsException e){ // поймали exception и дали ему имя "е"
+            System.out.println(e+ " Exception is catched");
+        }
+
+        finally {
+            System.out.println("finally is allready working");
+        }
+
+        System.out.println("Goodbye");
+
+    }
 }
---
+++ src/Lesson27/TryCatchFinally2.java
@@ -0,0 +1,26 @@
+package Lesson27;
+
+import java.io.*;
+
+public class TryCatchFinally2 {
+    public static void main(String[] args) throws Exception{
+        File f = new File("test10.txt");
+
+        try {
+            FileInputStream fls = new FileInputStream(f);
+            System.out.println("File is found!");
+            fls.read();
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File is not found!"); //поймали File is not found
+        }
+
+        finally {
+            System.out.println("Block finally is working");
+        }
+
+        FileOutputStream otfl = new FileOutputStream(f);
+        otfl.write(222);
+        System.out.println("Parameters are were recorded in "+f);
+    }
+}
