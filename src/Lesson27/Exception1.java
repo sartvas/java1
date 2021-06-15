d9f99fdfcb736edcd6f96663546dea4ab38762b0
Egor <sartvas@yandex.ru>
2021-06-15 16:21:17 +0300

    Lesson27:Introduce in Java.IO

---
+++ src/Lesson27/Exception1.java
@@ -0,0 +1,8 @@
+package Lesson27;
+
+public class Exception1 {
+    public static void main(String[] args) {
+        int array [] = {1 ,4, 6};
+        System.out.println(array[6]);
+    }
+}
---
+++ src/Lesson27/FilesIO.java
@@ -0,0 +1,20 @@
+package Lesson27;
+
+import java.io.*;
+
+public class FilesIO {
+    public static void main(String[] args) throws Exception{
+        // абстрактная репрезентация пути к файлу или папке
+        File f = new File("test10.txt");
+
+        //FileInputStream - создание потока для чтения из файла
+        FileInputStream fls = new FileInputStream(f);
+        System.out.println("File is found!");
+        fls.read();
+
+        //FileOutputStream - создание потока для записи в файл
+        FileOutputStream fls2 = new FileOutputStream(f);
+        fls2.write(100);
+
+    }
+}
