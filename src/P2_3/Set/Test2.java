968dfa4d3c988e5a5ed89f8d61811c0781114ecd
Egor <sartvas@yandex.ru>
2021-07-02 17:45:05 +0300

    All about TreeSet

--- src/P2_3/Set/Test2.java
+++ src/P2_3/Set/Test2.java
@@ -1,25 +1,72 @@
 package P2_3.Set;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+
+//В TreeSet нельзя добавить объект в виде ключа без имплиментации Comparable и
+// оверрайдинга compareTo, по выбранным параметрам
+// ВАЖНО также оверрайдить equals и hashCode по тем же параметрам, что и compareTo (см ниже)
 
 public class Test2 {
     public static void main(String[] args) {
-        HashSet <Integer> hs = new HashSet<>();
-        HashSet <Integer> hs2 = new HashSet<>();
-        HashSet <Integer> hsTest = new HashSet<>(hs);
+        TreeSet<Student> tset = new TreeSet<>();
+        Student st1 = new Student("Artom", 4);
+        Student st2 = new Student("Irina", 2);
+        Student st3 = new Student("Gennady", 1);
+        Student st4 = new Student("Den", 5);
+        Student st5 = new Student("Djohn", 1);
+        Student st6 = new Student("ivan", 5);
 
-        hs.add(1);
-        hs.add(2);
-        hs.add(3);
+        tset.add(st1);
+        tset.add(st2);
+        tset.add(st3);
+        tset.add(st4);
 
-        hs2.add(3);
-        hs2.add(6);
-        hs2.add(7);
+        System.out.println(tset);
+        System.out.println("Студент на первом месте: "+tset.first());
+        System.out.println("Студент на последнем месте: "+tset.last());
+        System.out.println("Студент, который меньше значения 'st5': "+tset.headSet(st5));
+        System.out.println("Студент, который выше значения 'st5': "+tset.tailSet(st5));
+        System.out.println("Студенты, которые между значениями 'st5' и 'st6': "+tset.subSet(st5,st6));
+    }
+}
 
-        hsTest.addAll(hs);
-        //hsTest.retainAll(hs2); - 3 пересечения
-        //в общем обычные методы коллекций
-        System.out.println(hsTest);
+class Student implements Comparable <Student>{
+    String name;
+    int course;
 
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name='" + name + '\'' + ", course=" + course + '}';
+    }
+
+
+    @Override
+    public int compareTo(@NotNull Student o) {
+        return this.course - o.course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Student student = (Student) o;
+        return course == student.course; //оставили только course, хотя был и name
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(course); //оставили только course, хотя был и name
     }
 }
