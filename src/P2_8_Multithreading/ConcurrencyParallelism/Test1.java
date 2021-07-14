a66dbbace20af8c5d2e290a3e8e3b3e722e04a62
Egor <sartvas@yandex.ru>
2021-07-14 17:20:51 +0300

    Mutitheading: cuncurrency, parallelism, synchronous

---
+++ src/P2_8_Multithreading/ConcurrencyParallelism/Test1.java
@@ -0,0 +1,7 @@
+package P2_8_Multithreading.ConcurrencyParallelism;
+
+//Cuncurrency  - выполнение нескольких задач, не обязательно в одно и то же время; (петь и кушать; говорить и готвить)
+//Parallelism - выполнение нескольких задач, в одно и то же время; частный случай Cuncurrency: говорить и готвить
+
+public class Test1 {
+}
---
+++ src/P2_8_Multithreading/Synchronous/Test1.java
@@ -0,0 +1,7 @@
+package P2_8_Multithreading.Synchronous;
+
+//Synchronous - последовательно, друг за другом: написание писем
+//Asynchronous - выполенние нескольких задач, можно одновременно - cuncurencyg
+
+public class Test1 {
+}
