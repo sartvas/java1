36f5499d805a5573b86ef1ff662df1782f7ec8d5
Egor <sartvas@yandex.ru>
2021-07-23 15:36:03 +0300

    Streams:FileInputStream & FileOutputStream

---
+++ src/Streams/Files.java
@@ -0,0 +1,20 @@
+package Streams;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Files {
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream("C:\\Users\\sartv\\OneDrive\\Изображения\\5ca0a1273067c57d7287062a438ba0e11.jpg");
+        FileOutputStream outputStream = new FileOutputStream("penoplex.jpg");
+        int i;
+        while ((i = inputStream.read()) != -1){
+            outputStream.write(i);
+        }
+        inputStream.close();
+        outputStream.close();
+        System.out.println("Done write");
+    }
+}
